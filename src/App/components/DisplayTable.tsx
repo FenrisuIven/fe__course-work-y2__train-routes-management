a20e6e86fcf6e98b427f724864318474824a0359
FenrisuIven <120093556+FenrisuIven@users.noreply.github.com>
2025-05-02 18:51:08 +0300

    basic data fetch logic, Mui DataGrid install

--- src/App/components/DisplayTable.tsx
+++ src/App/components/DisplayTable.tsx
@@ -1,11 +1,28 @@
 import Axios from 'axios';
+import {useEffect, useState} from "react";
 
-Axios.get("http://localhost:3000").then((response) => {
-  console.log({response});
-}).catch(() => {
-})
+import {DataGrid, GridRowsProp, GridColDef} from '@mui/x-data-grid';
 
 const DisplayTable = () => {
+  const [data, setData] = useState<GridRowsProp>()
+
+  useEffect(() => {
+    Axios.get("http://localhost:3000")
+      .then((response) => {
+        const rows: object[] = response.data;
+        if (Array.isArray(rows) && rows.length > 0 && Object.keys(rows[0]).length > 0) {
+          setData(rows);
+        }
+      })
+      .catch((e) => console.log(e))
+  }, []);
+
+  useEffect(() => {
+    if (data) {
+      console.log({data})
+    }
+  }, [data])
+
   return <></>
 }
 
